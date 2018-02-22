5f2b2a8dd2f97efb1d42ae3b9ec73a5cb1e890d6
Marcia <marciaandreacb@outlook.com>
2018-02-22 16:18:49 -0300

    Criacao da solucao

---
+++ InjecaoDependencia/InjecaoPropriedade/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InjecaoPropriedade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
